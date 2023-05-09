c8b4606d5b6375671b52dfde98a8c1ed19118c61
Kappaccinoh <lim.jia28@gmail.com>
2023-05-10 01:38:31 +0800

    24 medium

---
+++ 24.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        if (head.next == null) {
+            return head;
+        }
+
+        ListNode anchor = head.next.next;
+        ListNode first = head;
+        ListNode second = head.next;
+        second.next = head;
+        first.next = swapPairs(anchor);
+    
+        return second;
+    }
+}
